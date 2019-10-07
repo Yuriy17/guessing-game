5e296a7e59f5797188e58de98bfce0a3790116df
Yuriy17 <yuriy_sh@zoho.com>
2019-10-07 14:51:49 +0300

    guessing-game/feat: implement binary search

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,33 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.max;
+        this.min;
+        this.medium;
+    }
 
     setRange(min, max) {
-
+        this.min=min;
+        this.max=max;
     }
 
     guess() {
-
+        if(this.range==null){
+            this.medium = Math.ceil((this.min+this.max) / 2);
+            return this.medium;
+        }else{
+            return this.medium;
+        }
     }
-
-    lower() {
-
+    lower(){
+        this.max= this.medium;
+        this.medium = Math.ceil((this.min+this.medium)/2);
+        return this.medium;
     }
 
     greater() {
-
+        this.min= this.medium;
+        this.medium = Math.ceil((this.max+this.medium)/2);
+        return this.medium;
     }
 }
 
